abefc37fb562e1638d052d3d4e12e33140d79962
Sečkář Petr <petr.seckar@ysoft.com>
2024-09-14 22:24:34 +0200

    changed title and language, added redirect helper html for second domain

--- dist/admin.php
+++ dist/admin.php
@@ -28,7 +28,7 @@ $hideTime = isset($data['announcement']['hide_time']) ? $data['announcement']['h
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="cs">
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
